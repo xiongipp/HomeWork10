cc0e41443f3d5d51dae77a518b01f0a74cc691d3
xiongipp <39366100+xiongipp@users.noreply.github.com>
2019-10-16 21:16:51 +0800

    json字符串转xml字符串

--- src/JSONSTUDY/Test.java
+++ src/JSONSTUDY/Test.java
@@ -3,6 +3,8 @@ package JSONSTUDY;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import net.sf.json.xml.XMLSerializer;
 
 public class Test {
 
@@ -13,8 +15,17 @@ public class Test {
         String code=getCityCode.getCodeByName("仙桃");
         String url="http://t.weather.sojson.com/api/weather/city/"+code;
         String info=NetUtil.get(url);
+        XMLSerializer xmlSerializer=new XMLSerializer();
+        //json字符串转为xml字符串
+        String xml = xmlSerializer.write(JSONSerializer.toJSON(info));
+        System.out.println(xml);
+        //得到json对象，将需要的数据取出。
         JSONObject obj=JSONObject.fromObject(info);
-        return "查询城市为:"+obj.getString("cityInfo")+'\n'+"当前时间是"+obj.getString("date")+"当前天气信息："+obj.getString("data") ;
+        JSONObject cityName=JSONObject.fromObject(obj.getString("cityInfo"));
+        String location=cityName.getString("parent")+cityName.getString("city");
+        return "查询城市为:"+location+'\n'+
+                "当前时间是"+obj.getString("date")+'\n'+
+                "当前天气信息："+obj.getString("data") ;
     }
 
 }
